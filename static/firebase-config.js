0144ed4efcb22616de16e6551c2a9e61cdebf0a8
Allison Dakyung Kim <allykim526@gmail.com>
2026-08-08 22:47:09 +0900

    Create firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG4gvGfhYJhqyCi2ojLSyiDQVfQ1bVAxE",
+  authDomain: "project-fbdd9.firebaseapp.com",
+  projectId: "project-fbdd9",
+  storageBucket: "project-fbdd9.firebasestorage.app",
+  messagingSenderId: "582513846198",
+  appId: "1:582513846198:web:72d4a41f61473c7a088317",
+  measurementId: "G-8KP8VGKRP3",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
